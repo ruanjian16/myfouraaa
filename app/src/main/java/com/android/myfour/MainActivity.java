2f41066db32f020340f6b1df2d6217f43613a279
sxsoftware162 <sxsoftware162@163.com>
2018-09-06 15:51:32 +0800

    第二次提交

--- app/src/main/java/com/android/myfour/MainActivity.java
+++ app/src/main/java/com/android/myfour/MainActivity.java
@@ -2,9 +2,10 @@ package com.android.myfour;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+private TextView txt_1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
